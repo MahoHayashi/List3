d0b4d20e1e793052fa88121ad2d0bd3473570f44
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-07-11 23:25:24 +0900

    Finish project

--- List3/ContentView.swift
+++ List3/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count: Int = 0
+    @State var result = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(result)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 32))
+            Text("\(count)")
+                .frame(width: 300, height: 100)
+                .font(.system(size: 48))
+                .foregroundColor(count >= 0 ? .red : .blue)
+            //count >= 0がtrueの時.redでfalseの時.blue
+            HStack {
+                Button("+"){
+                    count = count + 1;
+                    if count >= 5 {
+                        result = "プラスの勝ち!"
+                    }else if count <= -5 {
+                        result = "マイナスの勝ち!"
+                    }else {
+                        result = ""
+                    }
+                }
+                .font(.system(size: 48))
+                .foregroundStyle(.white)
+                .frame(width: 100, height: 100)
+                .background(.red)
+                Button("-"){
+                    count = count - 1;
+                    if count >= 5 {
+                        result = "プラスの勝ち!"
+                    }else if count <= -5 {
+                        result = "マイナスの勝ち!"
+                    }else {
+                        result = ""
+                    }
+                }
+                .font(.system(size: 48))
+                .foregroundStyle(.white)
+                .frame(width: 100, height: 100)
+                .background(.blue)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
