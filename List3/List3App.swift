c9ab19b3b795dbfd3b215549e4f5a96746a327cd
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-07-11 20:16:09 +0900

    Initial Commit

---
+++ List3/List3App.swift
@@ -0,0 +1,17 @@
+//
+//  List3App.swift
+//  List3
+//
+//  Created by maho hayashi on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct List3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ List3Tests/List3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  List3Tests.swift
+//  List3Tests
+//
+//  Created by maho hayashi on 2025/07/11.
+//
+
+import Testing
+@testable import List3
+
+struct List3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
